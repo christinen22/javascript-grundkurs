258d3ffae755a2db74a713e58c411fa46b21bd6b
christinen22 <christine.nilsson@medieinstitutet.se>
2022-10-21 09:22:34 +0200

    first classes

---
+++ script.js
@@ -0,0 +1,70 @@
+alert("Hej JavaScript");
+
+console.log("Hello console!");
+
+let msg = "Voff voff voff";
+let cat = "Mjau";
+
+console.log(msg);
+
+console.log(cat);
+
+let firstName = "Christine";
+
+console.log(firstName);
+
+const lastName = "Nilsson";
+
+let fullName = firstName + lastName;
+
+console.log(fullName);
+
+firstName = "Sara";
+
+fullName = firstName + lastName;
+
+console.log(fullName);
+
+let helloMsg = "Hello" + firstName + " " + lastName;
+
+let myName = "Slim shady";
+console.log ("My name is: " + myName);
+
+console.log ("Lenght of name is: " + myName.length);
+
+console.log("First char in name is:", myName[0]);
+
+console.log("Name SHOUTED is:", myName.toUpperCase() );
+
+console.log("Name whispered is:", myName.toLowerCase() );
+
+console.log("Position of 'h'", myName.indexOf('h'));
+
+console.log("Slice of Slim Shady:", myName.slice(0, 2) ); "Sli"
+
+console.log("Does name contain 'e'?", myName.includes('e')); 
+
+
+
+let email = "johan.nordstrom@elevera.org";
+email = "jn@thehiveresistance.com";
+email = "johan.nordstrom@mil.gov.edu.com";
+
+// Skriv ut positionen för den sista punkten i variabeln `email`, oavsett hur lång/kort `email` är
+console.log("Last dot is at position:");
+
+console.log("The top domain is");
+
+
+
+
+
+
+
+students.push("Thanos"); //push lägger till sist i listan
+
+students.pop(); //pop tar bort det sista ur listan
+
+students.shift(); //shift tar bort det första ur listan
+
+students.unshift(Super-Man); //unshift lägger till först i listan
